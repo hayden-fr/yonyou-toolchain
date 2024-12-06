032dea01274975c39a137de8ba146bd3abc66708
hayden <48267247+hayden-fr@users.noreply.github.com>
2024-12-06 16:08:02 +0800

    Add feature of redirect / to default page

--- packages/plugin-ncc-multi-page/src/index.ts
+++ packages/plugin-ncc-multi-page/src/index.ts
@@ -1,7 +1,21 @@
-import { type Plugin } from "vite";
+import { type PluginOption } from 'vite'
 
-export default function multiPage(): Plugin {
-  return {
-    name: "vite-plugin-multi-page",
-  };
+export default function viteMultiPagePlugin(): PluginOption[] {
+  return [
+    {
+      name: 'vite-plugin-multi-page-server',
+      configureServer(server) {
+        server.middlewares.use(async (req, res, next) => {
+          const reqUrl = req.url
+
+          if (reqUrl === '/') {
+            res.writeHead(302, { location: '/nccloud' })
+            res.end()
+            return
+          }
+          next()
+        })
+      },
+    },
+  ]
 }
